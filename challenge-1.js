a5ac98663289fbc48aafa28dca5e3380a415bc01
Alex <a.lovinger.pountain@gmail.com>
2021-08-09 13:07:43 +0200

    challenge-1-explained

--- challenge-1.js
+++ challenge-1.js
@@ -4,3 +4,11 @@ let exampleArray = ['not','funny','but','not','correct','but','correct']
 let uniqueArray = [...new Set(exampleArray)]
 
 module.exports = uniqueArray
+
+
+
+
+/* 
+ Explanation:
+ I chose this method ("Set" + Spread Operator) as it's the simplest and cleanest I've found: using the "Set" object appears to be the most concise way to return unique values in the new array "uniqueArray". In this situation using "Set" + "Array.from" would be my next choice, which yields similar performace to using "Set" + Spread Operator.
+*/
