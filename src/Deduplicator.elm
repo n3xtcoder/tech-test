d1aa6d483d6d7f8fe3e8bb52da09c077db4207da
goat-fish <90009472+goat-duck@users.noreply.github.com>
2021-09-03 01:29:34 +0200

    Make the deduplicator deduplicate

--- src/Deduplicator.elm
+++ src/Deduplicator.elm
@@ -1,4 +1,15 @@
-module Deduplicator exposing (..)
+module Deduplicator exposing (deduplicate)
+
 
 deduplicate : List String -> List String
-deduplicate words = words
+deduplicate words =
+    List.foldl addToListIfNotMatch [] (List.sort words)
+
+addToListIfNotMatch : String -> List String -> List String
+addToListIfNotMatch element partialList =
+    if Just element /= List.head partialList then
+        element :: partialList
+
+    else
+        partialList
+
--- tests/DeduplicatorTest.elm
+++ tests/DeduplicatorTest.elm
@@ -1,27 +1,35 @@
 module DeduplicatorTest exposing (..)
 
 import Deduplicator
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Expect
 import Test exposing (..)
+
 
 suite : Test
 suite =
     describe "The Deduplicator module"
-        [ describe "Deduplicator.deduplicate" -- Nest as many descriptions as you like.
+        [ describe "Deduplicator.deduplicate"
+            -- Nest as many descriptions as you like.
             [ test "returns an identical list for an list of length 1" <|
                 \_ ->
                     let
                         words =
-                            ["fish"]
+                            [ "fish" ]
                     in
-                        Expect.equal words (Deduplicator.deduplicate words)
+                    Expect.equal words (Deduplicator.deduplicate words)
             , test "returns an identical list for an list of length 0" <|
                 \_ ->
                     let
                         words =
                             []
                     in
-                        Expect.equal words (Deduplicator.deduplicate words)
+                    Expect.equal words (Deduplicator.deduplicate words)
+            , test "returns a deduplicated list" <|
+                \_ ->
+                    let
+                        words =
+                            [ "fish", "chip", "egg", "fish", "egg" ]
+                    in
+                    Expect.equal [ "fish", "egg", "chip" ] (Deduplicator.deduplicate words)
             ]
         ]
